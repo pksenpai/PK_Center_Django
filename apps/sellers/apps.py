a8a7923fe7afa7788764e810d7e2f8fa2f743523
PKPY <p01001011p01011001@gmail.com>
2024-01-12 00:24:16 +0330

    [feat][project][apps]: add multiple languages feature for all apps.

--- apps/sellers/apps.py
+++ apps/sellers/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class SellersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.sellers'
+    verbose_name = _('Seller')
+    
