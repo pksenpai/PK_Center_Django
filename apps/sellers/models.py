b6a0ead81a4f3f8320192b4fe03f911939b6de67
PKPY <p01001011p01011001@gmail.com>
2024-01-15 13:36:36 +0330

    [dev][sellers][models]: create Seller model that inheritance from Profile Model from users app.

--- apps/sellers/models.py
+++ apps/sellers/models.py
@@ -1,3 +1,19 @@
+from apps.users.models import Profile
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class Seller(Profile):
+	rank = models.PositiveIntegerField(
+        null=True,
+        unique=True,
+        verbose_name=_("Rank")
+    )
+	
+	class Meta:
+		ordering = ("rank",)
+	
+	def __str__(self):
+		return self.name
+    
