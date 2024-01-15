f7f3dc6d14a8ebaf365886caced06360ebdf993b
PKPY <p01001011p01011001@gmail.com>
2024-01-15 13:39:40 +0330

    [dev][sellers][managers]: add & create custome manager for Seller model.

--- apps/sellers/models.py
+++ apps/sellers/models.py
@@ -1,19 +1,21 @@
 from apps.users.models import Profile
 from django.db import models
+from .managers import SellerManager
 
 from django.utils.translation import gettext_lazy as _
 
 
 class Seller(Profile):
-	rank = models.PositiveIntegerField(
+    rank = models.PositiveIntegerField(
         null=True,
         unique=True,
         verbose_name=_("Rank")
     )
-	
-	class Meta:
-		ordering = ("rank",)
-	
-	def __str__(self):
-		return self.name
-    
+
+    objects = SellerManager()
+
+    class Meta:
+        ordering = ("rank",)
+
+    def __str__(self):
+        return self.name
