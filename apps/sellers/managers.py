90e452cb86ed149444d80522a57e48b8d66fd8de
PKPY <p01001011p01011001@gmail.com>
2024-01-15 20:40:32 +0330

    [bugfix][sellers][managers]: bug: Seller table dont have any is_seller field. fix: use select related & lookups for relating to User model.

--- apps/sellers/managers.py
+++ apps/sellers/managers.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class SellerManager(models.Manager):
 	def get_queryset(self):
-		return super().get_queryset().filter(is_seller=True)
+		return super().get_queryset().select_related('user').filter(user__is_seller=True)
