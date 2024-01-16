c6fb78580552ada4592b6f83651637c731389a5c
PKPY <p01001011p01011001@gmail.com>
2024-01-16 20:20:54 +0330

    [dev][sellers][managers]: add more details to seller manager.

--- apps/sellers/managers.py
+++ apps/sellers/managers.py
@@ -2,5 +2,13 @@ from django.db import models
 
 
 class SellerManager(models.Manager):
+	"""
+	If the user is not an staff or superuser, but the user is an admin seller,
+	Then the profile is for the seller.
+    """
 	def get_queryset(self):
-		return super().get_queryset().select_related('user').filter(user__is_seller=True)
+		return super().get_queryset().select_related('user').filter(
+      		user__is_seller    = True,
+      		user__is_staff     = False,
+      		user__is_superuser = False,
+        ) # So it is an admin seller :)
