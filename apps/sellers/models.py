5b3eae898b44d76d2f7af3cbde3b9a5bdb9434c6
PKPY <p01001011p01011001@gmail.com>
2024-01-16 20:31:45 +0330

    [dev][sellers][models]: add category field to Seller model, sellers can have category for specify business context.

--- apps/sellers/models.py
+++ apps/sellers/models.py
@@ -1,5 +1,6 @@
-from apps.users.models import Profile
 from django.db import models
+from apps.users.models import Profile
+from apps.core.models import Category
 from .managers import SellerManager
 
 from django.utils.translation import gettext_lazy as _
@@ -7,11 +8,20 @@ from django.utils.translation import gettext_lazy as _
 
 class Seller(Profile):
     rank = models.PositiveIntegerField(
-        null=True,
-        unique=True,
-        verbose_name=_("Rank")
+        null         = True,
+        unique       = True,
+        verbose_name = _("Rank")
     )
 
+    category = models.ForeignKey(
+        to           = Category,
+        on_delete    = models.SET_NULL,
+        null         = True,
+        blank        = True,
+        related_name = "sellers",
+        verbose_name = _("Category"),
+    )
+    
     objects = SellerManager()
 
     class Meta:
