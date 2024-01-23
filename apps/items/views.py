3940c0c328f2b53b930567cea07d1959c601a64b
PKPY <p01001011p01011001@gmail.com>
2024-01-23 21:29:11 +0330

    [dev][items][views|templates]: wrote items list view for show items.

--- apps/items/views.py
+++ apps/items/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
+from django.db.models import Q
+
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 
+from .models import Item
 
-class ItemDetails(View):...
+
+class ItemsList(ListView):
+    model = Item
+    template_name = "items_list.html"
+    context_object_name = "items"
+    paginate_by = 20
+    
+    def get_queryset(self):
+        if searched:= self.request.GET.get("searched"):
+            searched: str = searched.strip() # remove extra space's :3
+            searched_result: queryset = self.model.objects.filter(
+                Q(name__icontains=searched) | Q(description__icontains=searched)
+            )
+            return searched_result
+        else:
+            return super().get_queryset().order_by('?')
+    
+
+class ItemDetails(DetailView):...
