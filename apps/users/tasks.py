e59a990e3e232aadd22e2a58f9e2521d739b1592
PKPY <p01001011p01011001@gmail.com>
2024-01-26 03:26:38 +0330

    [dev][users][tasks]: added two new task for send active user verification & celery beat schadule for delete user after 3 day if he/she didn't active her/his account.

--- apps/users/tasks.py
+++ apps/users/tasks.py
@@ -1,6 +1,8 @@
-from celery import shared_task
 from django.core.mail import send_mail
+from celery import shared_task
 from conf import settings
+from django.contrib.auth import get_user_model
+from django.utils import timezone as tz
 
 
 @shared_task(bind=True)
@@ -21,3 +23,33 @@ def send_otp_by_email(self, email, otp): # has self in args
     )
     
     return status
+
+@shared_task(bind=True)
+def send_verify_link(self, email, link):
+    mail_subject = " verify link "
+    message = \
+        "clik on this link!\n" \
+        f"link: http://127.0.0.1:8000{link}"
+
+    to_email = email
+
+    status = send_mail(
+        subject=mail_subject,
+        message=message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[to_email],
+        fail_silently=False,
+    )
+
+    return status
+
+@shared_task
+def delete_user():
+    users = get_user_model().objects.filter(is_active=False)
+    time_now = tz.now()
+    for user in users:
+        date_joined = user.date_joined
+        distance_time = time_now - date_joined
+        if distance_time >= tz.timedelta(seconds=60):
+            user.hard_delete()
+    
