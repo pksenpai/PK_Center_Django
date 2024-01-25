ba76ef7e3a75819ff6ff423b9ccf4975be31f784
PKPY <p01001011p01011001@gmail.com>
2024-01-26 03:28:40 +0330

    [dev][core][context]: added seller context for use it to filter items or more.

--- apps/core/contexts.py
+++ apps/core/contexts.py
@@ -1,6 +1,11 @@
 from .models import Category
+from apps.sellers.models import Seller
 
 
 def category(request):
     categories = Category.objects.all().order_by('name')
     return {"categories": categories}
+
+def seller(request):
+    sellers = Seller.objects.all().order_by('rank')
+    return {"sellers": sellers}
