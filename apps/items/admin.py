2e276550355b99313d6cbfa1f8953b0b3c8a19de
PKPY <p01001011p01011001@gmail.com>
2024-01-31 19:17:35 +0330

    [dev][items][admin]: add inline to items admin page.

--- apps/items/admin.py
+++ apps/items/admin.py
@@ -2,17 +2,54 @@ from django.contrib import admin
 from .models import *
 
 
+admin.site.register(ItemImage)
+admin.site.register(Rating)
+admin.site.register(SellerItem)
+admin.site.register(Favorite)
+
+class ItemImageInline(admin.TabularInline):
+    model = ItemImage
+    readonly_fields = ('id',)
+    extra = 1
+    
+
+class ItemRatingInline(admin.TabularInline):
+    model = Rating
+    readonly_fields = ('id', 'score', 'user')
+    extra = 1
+
+class ItemSellerItemInline(admin.TabularInline):
+    model = SellerItem
+    readonly_fields = ('id',)
+    extra = 1
+
+class ItemFavoriteInline(admin.TabularInline):
+    model = Favorite
+    readonly_fields = ('id', 'user')
+    extra = 1
+
+
+
 @admin.register(Item)
-class ItemAdmin(admin.ModelAdmin): ...
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [
+        ItemSellerItemInline,
+        ItemImageInline,
+        ItemRatingInline,
+        ItemFavoriteInline,
+    ]
 
-@admin.register(ItemImage)
-class ItemImageAdmin(admin.ModelAdmin): ...
+# @admin.register(Item)
+# class ItemAdmin(admin.ModelAdmin): ...
 
-@admin.register(Rating)
-class RatingAdmin(admin.ModelAdmin): ...
+# @admin.register(ItemImage)
+# class ItemImageAdmin(admin.ModelAdmin): ...
 
-@admin.register(SellerItem)
-class SellerAdmin(admin.ModelAdmin): ...
+# @admin.register(Rating)
+# class RatingAdmin(admin.ModelAdmin): ...
 
-@admin.register(Favorite)
-class FavoriteAdmin(admin.ModelAdmin): ...
+# @admin.register(SellerItem)
+# class SellerAdmin(admin.ModelAdmin): ...
+
+# @admin.register(Favorite)
+# class FavoriteAdmin(admin.ModelAdmin): ...
